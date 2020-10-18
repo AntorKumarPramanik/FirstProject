e7e410b10c02914cbf333d954df9cc897957f354
AntorKumarPramanik <73065739+AntorKumarPramanik@users.noreply.github.com>
2020-10-18 19:26:36 +0600

    STACK CODE ADDDED

---
+++ STACK/STACK (3).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int a,Amrito,amrito,b,a1,a2,Result,Remainder;
+    float d,c;
+    //a=47;
+    //b=9;
+    /*int counter;
+    for(counter=10;counter==0;--counter){
+       printf("Counter value is:%d\n",counter);
+    }*/
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        printf("value of i:%d\n",i);
+    }
+
+    return 0;
+}
---
+++ STACK/STACK (4).c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main(){
+    /*int a;
+    scanf("%d",&a);
+    if(a<0){
+        printf("%d is negative number\n",a);
+    }
+    */
+    /*
+    int num,remainder;
+    scanf("%d",&num);
+    remainder=num%2;
+    if(remainder==0)
+    {
+        printf("%d is even number\n",num);
+    }else{
+        printf("%d is odd number\n",num);
+    }*/
+    int userNumber=45;
+    switch(userNumber){
+    case 40:
+        printf("you are 40");
+    case 45:
+        printf("you are 45");
+    case 51:
+        printf("you are 51");
+    case 67:
+        printf("you are 67");
+    default:
+        printf("you are ")
+    }
+
+
+    return 0;
+
+
+}
